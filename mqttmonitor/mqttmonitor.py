ade96a05e963cfd179db9cf3f384c4ab89213a3c
Trammell hudson <hudson@trmm.net>
2019-12-09 19:04:59 +0100

    mqttmonitor: one message at a time, wrap and add ... to end

--- mqttmonitor/mqttmonitor.py
+++ mqttmonitor/mqttmonitor.py
@@ -4,7 +4,7 @@ from umqtt.simple import MQTTClient
 from machine import Pin, I2C
 import ssd1306
 
-mqtt_server = '192.168.0.10'
+mqtt_server = '192.168.0.44'
 wlan = network.WLAN()
 essid = wlan.config('essid')
 w = 128
@@ -31,9 +31,23 @@ print("ip: " + ip)
 
 def handle_msg(topic,msg):
 	print(topic + "=" + msg)
-	oled.scroll(0, -8)
-	oled.fill_rect(0, h-8, w, 8, 0)
-	oled.text(topic + "=" + msg, 0, h-8, 1)
+	s = topic + "=" + msg
+	l = int(w / 8) # chars per line
+
+	oled.fill(0)
+
+	if len(s) > 0*l:
+		oled.text(s[0*l:1*l], 0, 0*8)
+	if len(s) > 1*l:
+		oled.text(s[1*l:2*l], 0, 1*8)
+	if len(s) > 2*l:
+		oled.text(s[2*l:3*l], 0, 2*8)
+	if len(s) > 3*l:
+		oled.text(s[3*l:4*l], 0, 3*8)
+		if len(s) > 4*l:
+			oled.fill_rect(w-3*8, h-8, 3*8, h, 1)
+			oled.text("...", w-3*8, h-8, 0)
+
 	oled.show()
 
 mq = MQTTClient(str(wlan.config('mac')), mqtt_server)
